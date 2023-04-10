62664a0a83866b6e43d4ba242a16d59469b13102
amadeus1500 <130149201+amadeus1500@users.noreply.github.com>
2023-04-10 17:47:31 +0400

    Update ramen.js

--- js/ramen.js
+++ js/ramen.js
@@ -1,6 +1,6 @@
 $('.imagez img').click(function (e) { 
     e.preventDefault();
-    let carousel = document.querySelector('.carousel img').src
-    $('.carousel img').attr("src", this.src);
+    let carousel = document.querySelector('.carouseler img').src
+    $('.carouseler img').attr("src", this.src);
     $(this).attr("src", carousel);
 });
